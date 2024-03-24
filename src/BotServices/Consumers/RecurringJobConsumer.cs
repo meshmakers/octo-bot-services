9ceaa51c89edd023fda7af76683bd95ffacf5204
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-03-24 11:35:28 +0100

    AB#1088: New: Making jobs using masstransport working.

---
+++ src/BotServices/Consumers/RecurringJobConsumer.cs
@@ -0,0 +1,24 @@
+using Hangfire;
+using Hangfire.Storage;
+using Meshmakers.Octo.Common.DistributionEventHub.Consumers;
+using Meshmakers.Octo.Services.Common.DistributionEventHub.Commands;
+
+namespace Meshmakers.Octo.Backend.BotServices.Consumers;
+
+internal class RecurringJobConsumer : IDistributedConsumer<RemoveRecurringJobsByScheduleGroupRequest>
+{
+    public async Task ConsumeAsync(IDistributedContext<RemoveRecurringJobsByScheduleGroupRequest> context)
+    {
+        using var connection = JobStorage.Current.GetConnection();
+        
+        // Clean old jobs
+        foreach (var recurringJob in connection.GetRecurringJobs())
+        {
+            if (recurringJob.Id.EndsWith(context.Message.ScheduleGroup))
+            {
+                RecurringJob.RemoveIfExists(recurringJob.Id);
+            }
+        }
+        await context.RespondAsync(new GenericCommandResponse());
+    }
+}
