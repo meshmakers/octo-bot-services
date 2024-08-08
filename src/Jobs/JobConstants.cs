3e28376576a089af011eb02c5e00aba050b08f5e
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-08-08 10:35:26 +0200

    AB1441: New: Adapted import rt mechanism and adapted file name of exports (file in zip archive).

---
+++ src/Jobs/JobConstants.cs
@@ -0,0 +1,8 @@
+namespace Meshmakers.Octo.Backend.Jobs;
+
+internal class JobConstants
+{
+    public const string MimeTypeJson = "application/json";
+    public const string MimeTypeZip = "application/zip";
+    public const string MiemTypeYaml = "text/yaml";
+}
