87b80061856cc02aa6731b36d28b1f308932d818
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-12-23 08:52:43 +0100

    AB#3076: New: System construction kit version 2 with breaking changes to system queries

--- src/BotServices/Program.cs
+++ src/BotServices/Program.cs
@@ -92,7 +92,7 @@ try
 
     builder.Services.AddOctoJobs();
     builder.Services.AddOctoNotification();
-    builder.Services.AddCkModelSystemBotV1();
+    builder.Services.AddCkModelSystemBotV2();
 
     builder.Services.AddAuthentication(authenticationOptions =>
         {
